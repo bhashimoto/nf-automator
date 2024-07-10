1662e8b56a651f79ab8aa4857d01173407e7b403
bhashimoto <brunothashimoto@gmail.com>
2024-07-10 14:53:26 -0300

    added stdin support

--- src/main.py
+++ src/main.py
@@ -1,16 +1,25 @@
+#!/usr/bin/python3
 import sys
+import json
 from retriever import retrieve_data
 from drive_integration import import_data
 
 def main():
-    args = sys.argv
-    url:str = ''
-    if len(args) == 1:
-        url = input('please provide url: ')
+    urls = None
+    if sys.stdin:
+        urls = json.loads(sys.stdin.read())
     else:
-        url = args[1]
+        args = sys.argv
+        url:str = ''
+        if len(args) == 1:
+            url = input('please provide url: ')
+            urls = {url: url}
+        else:
+            url = args[1]
+            urls = {url: url}
 
-    data = retrieve_data(url)
-    import_data(data)
+    for url in urls.keys():
+        data = retrieve_data(url)
+        import_data(data)
 
 main()
